2f8dd1b80dc0e93f213d52fd7d35a9178986e81a
Dmytro Baturin <dbaturin02@gmail.com>
2025-07-06 16:03:42 +0200

    active line fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,20 @@
 const navLinks = document.querySelectorAll('.header__nav-link')
-
-let activeHash = window.location.hash.substring(1).split('#')[0]
+const activeLine = document.querySelector('.header__active-line')
 
 function updateActiveLink() {
-  activeHash = window.location.hash.substring(1).split('#')[0] || 'home'
+  const activeHash = window.location.hash.substring(1).split('#')[0] || 'home'
 
   navLinks.forEach(link => {
     const navAttribute = link.getAttribute('data-page')
 
     if (navAttribute === activeHash) {
       link.classList.add('header__nav-link--active')
+
+      const offsetLeft = link.offsetLeft
+      const width = link.offsetWidth
+
+      activeLine.style.width = `${width}px`
+      activeLine.style.transform = `translateX(${offsetLeft}px)`
     } else {
       link.classList.remove('header__nav-link--active')
     }
@@ -17,5 +22,5 @@ function updateActiveLink() {
 }
 
 updateActiveLink()
-
 window.addEventListener('hashchange', updateActiveLink)
+window.addEventListener('resize', updateActiveLink)
